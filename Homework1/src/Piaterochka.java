4eb8eb9a6e74f60909ffa596271a676d6160f1ce
Egor79-Kolola <egorkca79@gmail.com>
2022-11-01 11:53:45 +0300

    Create Piaterochka.java

---
+++ Homework1/src/Piaterochka.java
@@ -0,0 +1,9 @@
+public class Piaterochka {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i*5);
+            i++;
+        } while ((i*5)<=100);
+    }
+}
