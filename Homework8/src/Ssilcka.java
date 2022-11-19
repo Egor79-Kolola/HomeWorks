437165630cac23eaf79d1b4c919bfdab6522c464
Egor79-Kolola <egorkca79@gmail.com>
2022-11-19 21:18:35 +0300

    поиск ссылки

---
+++ Homework8/src/Ssilcka.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Ssilcka {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String link = "^http[s]?:\\//\\w+\\.com[/]?$";
+        System.out.println(Pattern.matches(link, str));
+    }
+}
