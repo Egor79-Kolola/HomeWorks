90eca06889642e7d7678864d8ce7d26edeb9cd26
Egor79-Kolola <egorkca79@gmail.com>
2022-11-20 10:08:57 +0300

    поиск подстроки

---
+++ Homework8/src/StringAB.java
@@ -0,0 +1,14 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAB {
+    public static void main(String[] args) {
+        String str = "ahb acb aeb aeeb adcb axeb";
+        Pattern pattern = Pattern.compile("a.b");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
+
+}
